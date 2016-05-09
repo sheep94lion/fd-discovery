f70633a458b3a72c0fc86ce847637fc7ecdd034d
ClearSpirit <xujingao13@gmail.com>
2016-05-09 11:00:33 +0800

    get partition

--- fd_node.py
+++ fd_node.py
@@ -51,10 +51,37 @@ def read_file():
         data.append(line_dict)
     return data
 
+
+def get_partition(data, list):
+    partition = {}
+    i = 1
+    for item_data in data:
+        attr = ''
+        for item_list in list:
+            attr += item_data[item_list]
+        if attr in partition:
+            partition[attr].append(i)
+        else:
+            partition[attr] = [i]
+        i += 1
+
+    return partition
+
+
+def is_fd(list1, list2, data):
+    l1 = len(get_partition(data, list1).keys())
+    list_union = list(set(list1).union(set(list2)))
+    l2 = len(get_partition(data, list_union).keys())
+    if l1 == l2:
+        return True
+    else:
+        return False
+
+
 data = read_file()
 print data[1]
 print len(data)
-
+print is_fd(['1', '2'], ['4', '5'], data)
 
 
 
