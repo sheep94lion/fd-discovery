ff70105d4176360c09b98d25e8071e979eae4c29
zhaoyi <zhaoyi.yuan31@gmail.com>
2016-05-09 21:58:21 +0800

    divided to two files

---
+++ fd_discovery.py
@@ -0,0 +1,48 @@
+def read_file():
+    file = open("data.txt")
+    data = []
+    while 1:
+        line = file.readline()
+        if not line:
+            break
+
+        line_list = line[0:-1].split(',')
+        line_dict = {}
+        i = 1
+        for item in line_list:
+            line_dict[str(i)] = item
+            i += 1
+        data.append(line_dict)
+    return data
+
+
+def get_partition(data, list):
+    partition = {}
+    i = 1
+    for item_data in data:
+        attr = ''
+        for item_list in list:
+            attr += item_data[item_list]
+        if attr in partition:
+            partition[attr].append(i)
+        else:
+            partition[attr] = [i]
+        i += 1
+
+    return partition
+
+
+def is_fd(list1, list2, data):
+    l1 = len(get_partition(data, list1).keys())
+    list_union = list(set(list1).union(set(list2)))
+    l2 = len(get_partition(data, list_union).keys())
+    if l1 == l2:
+        return True
+    else:
+        return False
+
+
+data = read_file()
+print data[1]
+print len(data)
+print is_fd(['1', '2'], ['4', '5'], data)
--- fd_node.py
+++ fd_node.py
@@ -43,55 +43,6 @@ def check_fd(x, y):
     return True
 
 
-def read_file():
-    file = open("data.txt")
-    data = []
-    while 1:
-        line = file.readline()
-        if not line:
-            break
-
-        line_list = line[0:-1].split(',')
-        line_dict = {}
-        i = 1
-        for item in line_list:
-            line_dict[str(i)] = item
-            i += 1
-        data.append(line_dict)
-    return data
-
-
-def get_partition(data, list):
-    partition = {}
-    i = 1
-    for item_data in data:
-        attr = ''
-        for item_list in list:
-            attr += item_data[item_list]
-        if attr in partition:
-            partition[attr].append(i)
-        else:
-            partition[attr] = [i]
-        i += 1
-
-    return partition
-
-
-def is_fd(list1, list2, data):
-    l1 = len(get_partition(data, list1).keys())
-    list_union = list(set(list1).union(set(list2)))
-    l2 = len(get_partition(data, list_union).keys())
-    if l1 == l2:
-        return True
-    else:
-        return False
-
-
-data = read_file()
-print data[1]
-print len(data)
-print is_fd(['1', '2'], ['4', '5'], data)
-
 def compute_dependencies_one_level(l, alphabet):
     for node in l.nodes:
         candidates = node.rhs_plus & node.attr_set
