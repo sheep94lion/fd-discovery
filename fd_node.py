1cde50a6639eeda3e02281cbd7d97c15bada343e
zhaoyi <zhaoyi.yuan31@gmail.com>
2016-05-09 21:26:02 +0800

    complete basically without test

--- fd_node.py
+++ fd_node.py
@@ -5,14 +5,18 @@ from functools import reduce
 class FdNode:
     def __init__(self, attr_list):
         self.attr_set = set(attr_list)
+        self.rhs_plus = set([])
 
 
 class Level:
     def __init__(self, n_level, alphabet, last_level):
+        self.level = n_level
         if n_level == 1:
             self.nodes = []
             for c in alphabet:
                 self.nodes.append(FdNode([c]))
+            for node in self.nodes:
+                node.rhs_plus = set(alphabet)
         else:
             self.nodes = generate_next_level(n_level, last_level)
 
@@ -31,10 +35,29 @@ def merge_nodes(nodes_list):
 def merge2node(node1, node2):
     node = FdNode([])
     node.attr_set = node1.attr_set | node2.attr_set
+    node.rhs_plus = node1.rhs_plus & node2.rhs_plus
     return node
 
 
+def check_fd(x, y):
+    return True
 
 
+def compute_dependencies_one_level(l, alphabet):
+    for node in l.nodes:
+        candidates = node.rhs_plus & node.attr_set
+        for e in candidates:
+            if check_fd(node.attr_set - {e}, {e}):
+                print(node.attr_set - {e}, e)
+                node.rhs_plus.remove(e)
+                node.rhs_plus = node.rhs_plus - (set(alphabet) - node.attr_set)
+    l.nodes = list(filter(lambda n: n.rhs_plus != set(), l.nodes))
+    return l
 
 
+def compute_dependencies(alphabet):
+    l = [Level(1, alphabet, 0)]
+    for i in range(len(alphabet)):
+        l[i] = compute_dependencies_one_level(l[i], alphabet)
+        if i < len(alphabet) - 1:
+            l.append(Level(i + 2, alphabet, l[i]))
