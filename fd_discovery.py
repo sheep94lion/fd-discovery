fcf95132240b3d25748b79ac86b302667ffc0095
ClearSpirit <xujingao13@gmail.com>
2016-05-11 15:54:58 +0800

    123

--- fd_discovery.py
+++ fd_discovery.py
@@ -1,5 +1,8 @@
+partition_list = {}
+
+
 def read_file():
-    file = open("test_data.txt")
+    file = open("data.txt")
     data = []
     while 1:
         line = file.readline()
@@ -19,24 +22,45 @@ def read_file():
 
 def get_partition(data, list):
     partition = {}
+    len = 0
     i = 1
     for item_data in data:
         attr = ''
         for item_list in list:
             attr += item_data[item_list]
+            attr += '+'
         if attr in partition:
             partition[attr].append(i)
         else:
             partition[attr] = [i]
+            len += 1
         i += 1
 
-    return partition
+    return len
+
+
+def get_partnum(list, data):
+    global partition_list
+    attr = ''
+    for item in sorted(list):
+        attr += str(item)
+        attr += '+'
+    if attr in partition_list:
+        l = partition_list[attr]
+        print 'touch'
+    else:
+        l = get_partition(data, list)
+        partition_list[attr] = l
+        print 'miss'
+
+    return l
 
 
 def is_fd(list1, list2, data):
-    l1 = len(get_partition(data, list1).keys())
+    l1 = get_partnum(list1, data)
     list_union = list(set(list1).union(set(list2)))
-    l2 = len(get_partition(data, list_union).keys())
+    l2 = get_partnum(list_union, data)
+
     if l1 == l2:
         return True
     else:
